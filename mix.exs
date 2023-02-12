69ce63485149f8f5d9c213e155fc9dd4c6f728be
Fernando Mendes <fernando@mendes.codes>
2023-02-12 00:08:32 +0000

    Bump version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SNS.MixProject do
   def project do
     [
       app: :sns,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
